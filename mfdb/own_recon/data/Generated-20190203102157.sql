9442a674dac78a165ef89df6f468db25e89fae73
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2019-02-03 17:33:18 +0100

    remove folder names

--- mfdb/own_recon/data/Generated-20190203102157.sql
+++ mfdb/own_recon/data/Generated-20190203102157.sql
@@ -1,5 +1,5 @@
 --------------------------------------------------------
 --  File created - Sunday-February-03-2019   
 --------------------------------------------------------
-@C:\git\mfdb\own_recon\data\RECON_FAQ_DATA_TABLE.sql
-@C:\git\mfdb\own_recon\data\TRANSL_REPOS_DATA_TABLE.sql
+@RECON_FAQ_DATA_TABLE.sql
+@TRANSL_REPOS_DATA_TABLE.sql
