3608eecbd49403a3c5a8a2308ffb3d332f9985fd
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2019-02-03 17:31:04 +0100

    added folder names and use new extensions

--- mfdb/own_reporting/install.sql
+++ mfdb/own_reporting/install.sql
@@ -7,137 +7,137 @@ prompt
 prompt Creating table MFR_DOM_DATA_MIGR_STAGE
 prompt ======================================
 prompt
-@@mfr_dom_data_migr_stage.tab
+@@table/mfr_dom_data_migr_stage.tab
 prompt
 prompt Creating table MFR_DOM_FINDING_SEVERITY
 prompt =======================================
 prompt
-@@mfr_dom_finding_severity.tab
+@@table/mfr_dom_finding_severity.tab
 prompt
 prompt Creating table MFR_DOM_PROGRAM_TYPE
 prompt ===================================
 prompt
-@@mfr_dom_program_type.tab
+@@table/mfr_dom_program_type.tab
 prompt
 prompt Creating table MFR_DOM_TQC_SCOPE_TYPE
 prompt =====================================
 prompt
-@@mfr_dom_tqc_scope_type.tab
+@@table/mfr_dom_tqc_scope_type.tab
 prompt
 prompt Creating table MFR_SUBJECT_MATTER
 prompt =================================
 prompt
-@@mfr_subject_matter.tab
+@@table/mfr_subject_matter.tab
 prompt
 prompt Creating table MFR_TECH_QLTY_CHK_SCOPE
 prompt ======================================
 prompt
-@@mfr_tech_qlty_chk_scope.tab
+@@table/mfr_tech_qlty_chk_scope.tab
 prompt
 prompt Creating table MFR_PROGRAM
 prompt ==========================
 prompt
-@@mfr_program.tab
+@@table/mfr_program.tab
 prompt
 prompt Creating table MFR_PROCESS
 prompt ==========================
 prompt
-@@mfr_process.tab
+@@table/mfr_process.tab
 prompt
 prompt Creating table MFR_PROCESS_FINDING
 prompt ==================================
 prompt
-@@mfr_process_finding.tab
+@@table/mfr_process_finding.tab
 prompt
 prompt Creating table MFR_PROCESS_RESULT
 prompt =================================
 prompt
-@@mfr_process_result.tab
+@@table/mfr_process_result.tab
 prompt
 prompt Creating table MFR_SCOPE_FINDING
 prompt ================================
 prompt
-@@mfr_scope_finding.tab
+@@table/mfr_scope_finding.tab
 prompt
 prompt Creating sequence MFR_PROCESS_FINDING_FND_ID_SEQ
 prompt ================================================
 prompt
-@@mfr_process_finding_fnd_id_seq.seq
+@@sequence/mfr_process_finding_fnd_id_seq.seq
 prompt
 prompt Creating sequence MFR_PROCESS_PROCESS_ID_SEQ
 prompt ============================================
 prompt
-@@mfr_process_process_id_seq.seq
+@@sequence/mfr_process_process_id_seq.seq
 prompt
 prompt Creating view MFR_DOM_DATA_MIGR_STAGE_V
 prompt =======================================
 prompt
-@@mfr_dom_data_migr_stage_v.vw
+@@view/mfr_dom_data_migr_stage_v.vw
 prompt
 prompt Creating view MFR_DOM_FINDING_SEVERITY_V
 prompt ========================================
 prompt
-@@mfr_dom_finding_severity_v.vw
+@@view/mfr_dom_finding_severity_v.vw
 prompt
 prompt Creating view MFR_DOM_PROGRAM_TYPE_V
 prompt ====================================
 prompt
-@@mfr_dom_program_type_v.vw
+@@view/mfr_dom_program_type_v.vw
 prompt
 prompt Creating view MFR_DOM_TQC_SCOPE_TYPE_V
 prompt ======================================
 prompt
-@@mfr_dom_tqc_scope_type_v.vw
+@@view/mfr_dom_tqc_scope_type_v.vw
 prompt
 prompt Creating view MFR_PROCESS_FINDING_V
 prompt ===================================
 prompt
-@@mfr_process_finding_v.vw
+@@view/mfr_process_finding_v.vw
 prompt
 prompt Creating view MFR_PROCESS_RESULT_V
 prompt ==================================
 prompt
-@@mfr_process_result_v.vw
+@@view/mfr_process_result_v.vw
 prompt
 prompt Creating view MFR_PROCESS_V
 prompt ===========================
 prompt
-@@mfr_process_v.vw
+@@view/mfr_process_v.vw
 prompt
 prompt Creating view MFR_PROGRAM_V
 prompt ===========================
 prompt
-@@mfr_program_v.vw
+@@view/mfr_program_v.vw
 prompt
 prompt Creating view MFR_SCOPE_FINDING_V
 prompt =================================
 prompt
-@@mfr_scope_finding_v.vw
+@@view/mfr_scope_finding_v.vw
 prompt
 prompt Creating view MFR_SUBJECT_MATTER_V
 prompt ==================================
 prompt
-@@mfr_subject_matter_v.vw
+@@view/mfr_subject_matter_v.vw
 prompt
 prompt Creating view MFR_TECH_QLTY_CHK_SCOPE_V
 prompt =======================================
 prompt
-@@mfr_tech_qlty_chk_scope_v.vw
+@@view/mfr_tech_qlty_chk_scope_v.vw
 prompt
 prompt Creating package MFR_REPORT_CAPTURING_API
 prompt =========================================
 prompt
-@@mfr_report_capturing_api.spc
+@@package/mfr_report_capturing_api.pks
 prompt
 prompt Creating procedure DELETE_FOR_TESTING
 prompt =====================================
 prompt
-@@delete_for_testing.prc
+@@procedure/delete_for_testing.prc
 prompt
 prompt Creating package body MFR_REPORT_CAPTURING_API
 prompt ==============================================
 prompt
-@@mfr_report_capturing_api.pks
+@@package/mfr_report_capturing_api.pkb
 
 prompt Done
 spool off
